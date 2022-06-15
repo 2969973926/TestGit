bac9869fddc037daca971e72abd68c46f8ea799e
admin <admin@gjy.com>
2022-06-15 14:41:14 +0800

    first...commit

--- src/main/java/com/gjy/git/controller/TestController.java
+++ src/main/java/com/gjy/git/controller/TestController.java
@@ -3,5 +3,6 @@ package com.gjy.git.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("git1...........");
+        System.out.println("git2.......");
     }
 }
